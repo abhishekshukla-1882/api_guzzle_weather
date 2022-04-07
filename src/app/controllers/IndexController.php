49e21647c77c206ff1e331e967cf6e97fc7a1f02
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-04-07 12:08:28 +0530

    done

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -70,7 +70,7 @@ class IndexController extends Controller
         // echo $location;
         // die;
         if($forcast){
-            $url = "http://api.weatherapi.com/v1/forecast.json?key= 0bab7dd1bacc418689b143833220304&q=$forcast&days=2&aqi=no&alerts=no";
+            $url = "http://api.weatherapi.com/v1/forecast.json?key= 0bab7dd1bacc418689b143833220304&q=$forcast&days=3&aqi=no&alerts=no";
         }
         $client = new Client([
             'base_uri' => "$url",
@@ -164,10 +164,56 @@ class IndexController extends Controller
         $body = $response->getBody();
         $body = json_decode($body,1);
         // echo "<pre>";
-        // print_r($body);
+        // print_r($body['golf'][0]['country']);
         // die;
         $this->view->sport = $body;
 
 
     }
+    public function airAction(){
+        $client = new Client();
+
+        $city = $this->request->getPost('city');
+        // echo $city;
+        // die;
+        if($city){
+            $url = "http://api.weatherapi.com/v1/current.json?key= 0bab7dd1bacc418689b143833220304&q=$city&aqi=no";
+        }
+        $client = new Client([
+            'base_uri' => "$url",
+        ]);
+        $response = $client->request('GET');
+        $body = $response->getBody();
+        $body = json_decode($body,1);
+        // echo "<pre>";
+        // print_r($body);
+        // die;
+        $this->view->air = $body;
+
+
+    }
+    public function alertAction(){
+        $client = new Client();
+
+        $forcast = $this->request->getPost('alert');
+        // echo $forcast;
+        // die;
+        if($forcast){
+            $url = "http://api.weatherapi.com/v1/forecast.json?key= 0bab7dd1bacc418689b143833220304&q=$forcast&days=2&aqi=no&alerts=yes";
+        }
+        $client = new Client([
+            'base_uri' => "$url",
+        ]);
+        $response = $client->request('GET');
+        $body = $response->getBody();
+        $body = json_decode($body,1);
+        // echo "<pre>";
+        // print_r($body['alerts']['alert']);
+        // echo count($body['alerts']['alert']);
+        // die;
+        $this->view->air = $body;
+    }
+
+
+    
 }
